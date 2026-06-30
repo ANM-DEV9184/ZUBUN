4e7b4bfbf3d4271f5a712a9f363ea1bf6a05d451
GameIT <saasbyanm@gmail.com>
2026-07-01 01:11:10 +0400

    Initial Commit

---
+++ ZUBUN/ZUBUNApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZUBUNApp.swift
+//  ZUBUN
+//
+//  Created by Abdullah on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ZUBUNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZUBUNUITests/ZUBUNUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  ZUBUNUITestsLaunchTests.swift
+//  ZUBUNUITests
+//
+//  Created by Abdullah on 01/07/2026.
+//
+
+import XCTest
+
+final class ZUBUNUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
